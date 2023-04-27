1e2f1acf0aa772b02e7591070cc8c6175d7a2e0e
get <45425365+Minty-Meeo@users.noreply.github.com>
2023-04-27 11:30:56 -0500

    Update Util.h

--- Source/Core/Util.h
+++ Source/Core/Util.h
@@ -1,68 +1,75 @@
 #pragma once
 
 #include <charconv>
+#include <concepts>
 #include <iterator>
 #include <string_view>
 #include <utility>
 
 namespace util
 {
-template <typename T>
-inline T __svto(const std::string_view sv, std::size_t* pos = nullptr, int base = 10)
+template <std::integral T>
+inline T svto(const std::string_view sv, std::size_t* pos = nullptr, const int base = 10) noexcept
 {
-  const char *first = sv.data(), *last = first + sv.size();
   T value{};
-  const char* ptr = std::from_chars(first, last, value, base).ptr;
+  const char* const head = sv.begin();
+  const char* const tail = sv.end();
+  const char* const ptr = std::from_chars(head, tail, value, base).ptr;
   // Don't bother throwing std::invalid_argument or std::out_of_range.  I never write bad code :^)
   if (pos != nullptr)
-    *pos = std::distance(first, ptr);
+    *pos = std::distance(head, ptr);
   return value;
 }
 
-inline long svtoi(const std::string_view sv, std::size_t* pos = nullptr, int base = 10)
+inline long svtoi(const std::string_view sv, std::size_t* pos = nullptr,
+                  const int base = 10) noexcept
 {
-  return __svto<int>(sv, pos, base);
+  return svto<int>(sv, pos, base);
 }
-inline long svtol(const std::string_view sv, std::size_t* pos = nullptr, int base = 10)
+inline long svtol(const std::string_view sv, std::size_t* pos = nullptr,
+                  const int base = 10) noexcept
 {
-  return __svto<long>(sv, pos, base);
+  return svto<long>(sv, pos, base);
 }
-inline long svtoll(const std::string_view sv, std::size_t* pos = nullptr, int base = 10)
+inline long svtoll(const std::string_view sv, std::size_t* pos = nullptr,
+                   const int base = 10) noexcept
 {
-  return __svto<long long>(sv, pos, base);
+  return svto<long long>(sv, pos, base);
 }
-inline long svtoul(const std::string_view sv, std::size_t* pos = nullptr, int base = 10)
+inline long svtoul(const std::string_view sv, std::size_t* pos = nullptr,
+                   const int base = 10) noexcept
 {
-  return __svto<unsigned long>(sv, pos, base);
+  return svto<unsigned long>(sv, pos, base);
 }
-inline long svtoull(const std::string_view sv, std::size_t* pos = nullptr, int base = 10)
+inline long svtoull(const std::string_view sv, std::size_t* pos = nullptr,
+                    const int base = 10) noexcept
 {
-  return __svto<unsigned long long>(sv, pos, base);
+  return svto<unsigned long long>(sv, pos, base);
 }
 
 // https://lists.isocpp.org/std-proposals/att-0008/Dxxxx_string_view_support_for_regex.pdf
 
 // This code assumes you are passing in a valid std::csub_match from a std::cmatch_results.
-constexpr static std::string_view  //
-to_string_view(const std::pair<const char*, const char*>& pair)
+constexpr static std::string_view
+to_string_view(const std::pair<const char*, const char*>& pair) noexcept
 {
   return {pair.first, pair.second};
 }
 // This code assumes you are passing in a valid std::ssub_match from a std::smatch_results.
-constexpr static std::string_view
-to_string_view(const std::pair<std::string::const_iterator, std::string::const_iterator>& pair)
+constexpr static std::string_view to_string_view(
+    const std::pair<std::string::const_iterator, std::string::const_iterator>& pair) noexcept
 {
   return {pair.first, pair.second};
 }
 // This code assumes you are passing in a valid std::wcsub_match from a std::wcmatch_results.
 constexpr static std::wstring_view
-to_wstring_view(const std::pair<const wchar_t*, const wchar_t*>& pair)
+to_wstring_view(const std::pair<const wchar_t*, const wchar_t*>& pair) noexcept
 {
   return {pair.first, pair.second};
 }
 // This code assumes you are passing in a valid std::wssub_match from a std::wsmatch_results.
-constexpr static std::wstring_view
-to_wstring_view(const std::pair<std::wstring::const_iterator, std::wstring::const_iterator>& pair)
+constexpr static std::wstring_view to_wstring_view(
+    const std::pair<std::wstring::const_iterator, std::wstring::const_iterator>& pair) noexcept
 {
   return {pair.first, pair.second};
 }
